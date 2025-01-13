3af92737b7da3b143a2b7302fb7df7e6b27c1f36
許睿舜 <steven921107@yahoo.com.tw>
2025-01-14 01:38:50 +0800

    Updating our view

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -12,7 +12,7 @@ class Chirp extends Model
     protected $fillable = [
         'message',
     ];
-    
+
     public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
